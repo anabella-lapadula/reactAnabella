108cc05350a5d860b1e8016e0ab11509d2f2d2bc
Anabella La Padula <anabella.lapadula@gmail.com>
2023-01-23 13:40:34 -0300

    Correccion Ubicacion Carpeta

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './componentes/NavBar/NavBar'
-import ItemListContainer from './componentes/CartWidget/ItemListConteiner/ItemListContainer';
+import ItemListContainer from './componentes/ItemListConteiner/ItemListContainer';
 
 
 function App(props) {
